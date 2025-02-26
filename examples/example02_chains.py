124b5822d5658459afe185d6a6e1e373984c558b
LockeErasmus <22911835+LockeErasmus@users.noreply.github.com>
2025-02-26 09:34:59 +0100

    chain distribution update

--- examples/example02_chains.py
+++ examples/example02_chains.py
@@ -5,7 +5,7 @@ import logging
 
 import numpy as np
 
-region = [-4.5, 2.5, 0, 800]
+region = [-4.5, 2.5, 0, 100]
 delays = np.array([24.99, 23.35, 19.9, 18.52, 13.32, 10.33, 8.52, 4.61, 0.0])
 coefs = np.array([[51.7, 0, 0, 0, 0, 0, 0, 0 , 0],
                     [1.5, -0.1, 0.04, 0.03, 0, 0, 0, 0, 0],
@@ -24,6 +24,21 @@ if __name__ == "__main__":
 
     from qpmr.distribution import _distribution_diagram
 
-    logger = qpmr.init_logger(level="DEBUG")
+    logger = qpmr.init_logger(level="DEBUG", format="%(name)s - %(message)s")
 
-    _distribution_diagram(coefs, delays)
+    roots, meta = qpmr.qpmr(region, coefs, delays)
+    mi, omega = _distribution_diagram(coefs, delays, assume_compressed=False)
+
+    beta = np.linspace(region[0], region[1], 1000)
+    fig, ax = plt.subplots(1,1)
+    for m, w in zip(mi, omega):
+        for wk in w: 
+            plt.plot(beta, wk*np.exp(-1/m*beta),"k--", alpha=0.4)
+    ax = qpmr.plot.roots_basic(roots, ax=ax)
+    plt.xlim(region[0], region[1])
+    plt.ylim(region[2], region[3])
+    plt.show()
+        
+
+
+    
--- src/qpmr/distribution.py
+++ src/qpmr/distribution.py
@@ -1,5 +1,9 @@
 """
-Set of functions for constructing distribution diagram
+Set of functions for examining distribution of zeros
+----------------------------------------------------
+1. distribution diagram
+2. zeros-chain "asymptotes"
+3. areas free of zeros - TODO
 """
 
 import logging
@@ -8,11 +12,11 @@ import numpy as np
 import numpy.typing as npt
 
 from .quasipoly import QuasiPolynomial
-from .quasipoly.core import poly_degree
+from .quasipoly.core import poly_degree, compress
 
 logger = logging.getLogger(__name__)
 
-def concave_envelope_inplace(x: npt.NDArray, y: npt.NDArray, mask: npt.NDArray) -> None:
+def _concave_envelope_inplace(x: npt.NDArray, y: npt.NDArray, mask: npt.NDArray) -> None:
     """ inplace fills the mask with True values representing concave envelope
 
     Args:
@@ -41,30 +45,62 @@ def concave_envelope_inplace(x: npt.NDArray, y: npt.NDArray, mask: npt.NDArray) 
         i = np.argmax(distance)
         if distance[i] >= 0.0: # extra step is needed, also, i is not index of boundaries
             mask[i+1] = True
-            concave_envelope_inplace(x[:i+2], y[:i+2], mask[:i+2])
-            concave_envelope_inplace(x[i+1:], y[i+1:], mask[i+1:])
+            _concave_envelope_inplace(x[:i+2], y[:i+2], mask[:i+2])
+            _concave_envelope_inplace(x[i+1:], y[i+1:], mask[i+1:])
 
-def _distribution_diagram(coefs, delays, **kwargs):
+def _concave_envelope(x: npt.NDArray, y: npt.NDArray) -> npt.NDArray:
+    """ Creates concave envelope mask
 
-    # TODO has to be in compressed form !!! TODO
-    print(f"{delays=}")
-
-    degrees = np.apply_along_axis(poly_degree, 1, coefs)
-
-    # highest degree coefficient - 1D array
-    coef_hdeg = coefs[np.arange(0, len(coefs)), degrees.astype(int)]
-    thetas = -delays + np.max(delays)
-
-    print(f"{degrees=}")
-    print(f"{coef_hdeg=}")
-    print(f"{thetas=}")
-
-    # form concave envelope
-    # concave envelope of x=thetas[::-1], y=degrees[::-1]
-    mask = np.full_like(thetas, fill_value=False, dtype=bool)
+    Args:
+        x (array): x coordinates (thetas) 
+        y (array): y coordinates (degrees)
+    
+    Returns:
+        mask (array): mask defining which [x,y] forms the envelope
+    """
+    if x.ndim != 1 or y.ndim != 1:
+        raise ValueError("Inputs have to 1D arrays")
+    if x.shape != y.shape:
+        raise ValueError(f"Length of `x` and `y` has to match")
+    if x.size == 0: # Return None if empty
+        return 
+    
+    mask = np.full_like(x, fill_value=False, dtype=bool)
     mask[0] = True
     mask[-1] = True
-    concave_envelope_inplace(thetas, degrees, mask)
+    _concave_envelope_inplace(x,y,mask)
+    return mask
+
+
+def _distribution_diagram(coefs, delays, **kwargs):
+    """ TODO
+    
+    Args:
+        coefs (array): matrix definition of polynomial coefficients (each row
+            represents polynomial coefficients corresponding to delay)
+        delays (array): vector definition of associated delays (each delay
+            corresponds to row in `coefs`)
+        **kwargs:
+            assume_compressed (bool): set to True if quasipolynomial already
+                in compressed form, default False
+    Returns:
+        TODO
+    """
+    assume_compressed = kwargs.get("assume_compressed", False)
+    if assume_compressed:
+        logger.debug("Not checking if input quasipolynomial is ")
+    else: # perform compression and delay sorting
+        coefs, delays = compress(coefs, delays)
+    
+    # 1D vector representing the degree of polynomials
+    degrees = np.apply_along_axis(poly_degree, 1, coefs[::-1, :])
+    # highest degree coefficient and thetas
+    coef_hdeg = (coefs[np.arange(len(coefs)-1,-1,-1), degrees.astype(int)])
+    thetas = -delays[::-1] + np.max(delays)
+
+    # form concave envelope (represented by mask), i.e.
+    # envelope [x,y] is formed via thetas[mask], degrees[mask] 
+    mask = _concave_envelope(thetas, degrees)
     
     print(f"{mask=}")
     print(f"{degrees[mask]=}")
@@ -72,34 +108,58 @@ def _distribution_diagram(coefs, delays, **kwargs):
     
     ix = np.arange(0, len(mask))[mask]
     n_segments = np.sum(mask) - 1
+    mi_vec = np.zeros(shape=(n_segments,), dtype=np.float64) # storing mi
+    poly_fw = [] # container for associated polynomial representation
+    roots = [] # container for roots
+    abs_omega = []
+
     logger.debug(f"Number of segments: {n_segments}") # TODO what if one delay only
     for k in range(n_segments): # iterate over all segments
         logger.debug(f"Working on segment {k} -> spaning {ix[k]} - {ix[k+1]}")
 
         # TODO: vectorize
         segment_mi = (degrees[ix[k+1]] - degrees[ix[k]]) / (thetas[ix[k+1]] - thetas[ix[k]]) # slope
-        print(f"{segment_mi=}")
+        mi_vec[k] = segment_mi
 
-        if segment_mi == 0.0: # neutral segment
+        if segment_mi == 0.0: # this is neutral segment
+            logger.debug(f"Neutral segment")
             # TODO
-            pass
         else:
             # construct coefficients of polynomial fr(w)
-            m0 = degrees[ix[k]]
-            deg_ = degrees[ix[k]:ix[k+1]+1] - m0
-            coef_ = coef_hdeg[ix[k]:ix[k+1]+1]
+            m0 = degrees[ix[k]] # degree of left P
+            # all Points P with relative degrees and coefficients associated
+            # with the highest power s
+            segment_rel_deg = degrees[ix[k]:ix[k+1]+1] - m0
+            segment_coef_all = coef_hdeg[ix[k]:ix[k+1]+1]
 
-            print(deg_, coef_)
+            segment_coef = np.zeros(shape=(np.max(segment_rel_deg)+1))
+            # pick correct coefficients and form polynomial fr(w) representation
+            prev_d = -1 # first d=0
+            for d,c in zip(segment_rel_deg, segment_coef_all):
+                if d > prev_d:
+                    # adding to representation
+                    segment_coef[d] = c
+                    prev_d = d
+            # TODO minimal form of polynomial ? or unnecessary?
+            segment_roots = np.roots(segment_coef[::-1])
+            wk = np.round(np.abs(segment_roots), decimals=10)
+            wk = np.unique(wk)
+            logger.debug(f"{segment_coef=}, {segment_roots=}, |wk|={wk}")
+            poly_fw.append(segment_coef)
+            roots.append(segment_roots)
 
-            segment_coef = np.zeros(shape=(np.max(deg_)+1))
-            segment_coef[0] = coef_hdeg[ix[k]]
-            for i in range(1, np.max(deg_) + 1):
-                print(i)
-                segment_coef[i] = np.sum(coef_[deg_ == (i - 1)])
+            # we need only the roots with unique absolute value
+            abs_omega.append(wk)
+    
+    return mi_vec, abs_omega
 
-        logger.debug(f"{segment_coef=}")
 
-        raise NotImplementedError(".")
+def _chain_asymptotes():
+    """
+    
+    """
+
+
 
 def distribution_diagram(qp: QuasiPolynomial, assume_minimal=False) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
     """ Creates distribution diagram for quasipolynomial
@@ -133,10 +193,7 @@ def distribution_diagram(qp: QuasiPolynomial, assume_minimal=False) -> tuple[npt
     thetas = alpha0 - delays # positive numbers, last one should be 0.0
 
     # concave envelope of x=thetas[::-1], y=degrees[::-1]
-    mask = np.full_like(thetas, fill_value=False, dtype=bool)
-    mask[0] = True
-    mask[-1] = True
-    concave_envelope_inplace(thetas[::-1], degrees[::-1], mask)
+    mask = _concave_envelope(thetas[::-1], degrees[::-1], mask)
     
     return thetas[::-1], degrees[::-1], mask
 
