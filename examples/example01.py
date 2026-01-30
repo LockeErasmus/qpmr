add4f48c9996aa3c85d6403cb06887be38138b6e
LockeErasmus <22911835+LockeErasmus@users.noreply.github.com>
2026-01-30 10:24:06 +0100

    WIP github page

--- examples/example01.py
+++ examples/example01.py
@@ -2,6 +2,13 @@
 Example 1
 =========
 
+We start with he introductory quasi-polynomial
+
+.. math::
+
+    h(s) = e
+
+
 
 
 """
@@ -15,7 +22,7 @@ import qpmr.plot
 delays = np.array([0.0, 1.0])
 coefs = np.array([[0., 1],[1, 0]])
 
-roots, meta = qpmr.qpmr(coefs, delays, [-10, 2, 0, 30])
+roots, meta = qpmr.qpmr(coefs, delays, region=[-10, 2, 0, 30])
 
 qpmr.plot.roots(roots)
 plt.show()
